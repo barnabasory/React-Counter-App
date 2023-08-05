27dddf21d767cb8d3de6c60ba3b9d24f3d96e0a8
barnabas2022 <123511857+barnabas2022@users.noreply.github.com>
2023-08-05 10:43:11 +0100

    add React Counter App to projects folder

---
+++ src/exports.js
@@ -0,0 +1,7 @@
+export { default as ReactCounter } from "./projects/ReactCounter";
+export { default as ImageCarousel } from "./projects/ImageCarousel";
+
+//images
+export { default as Image1 } from "./images/image1.jpeg";
+export { default as Image2 } from "./images/image2.jpeg";
+export { default as Image3 } from "./images/image3.jpeg";
